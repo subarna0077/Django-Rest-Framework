edf249b586d1b9c18b9f016048687d7fd4f2c371
subh077 <88318416+subarna0077@users.noreply.github.com>
2025-08-06 14:48:45 +0545

    Learned the custom serializer to add extra data like aggregation, count etc

--- Video 1/api/serializers.py
+++ Video 1/api/serializers.py
@@ -26,7 +26,18 @@ class OrderItemSerializer(serializers.ModelSerializer):
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
     class Meta:
         model = Order
-        fields = ['order_id', 'status', 'created_at', 'items']
+        fields = ['order_id', 'status', 'created_at', 'items', 'total_price']
 
+
+class ProductInfoSerializer(serializers.Serializer):
+    #get all products, count of products, max price
+    products = ProductSerializer(many=True)
+    count = serializers.IntegerField()
+    max_price = serializers.FloatField()
